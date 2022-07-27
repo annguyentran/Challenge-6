d67716a00fae0e3479dbeb064b7a7396876141c3
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-07-26 17:50:08 -0700

    challenge 6 first push

---
+++ logic.js
@@ -0,0 +1,27 @@
+function getinfo() {
+const cityInputEl = document.getElementById("cityInput")
+const cityNameEl = document.getElementById("cityName")
+cityNameEl.innerHTML = cityInputEl.value
+
+}
+
+let weather = {
+
+function: weatherFetch(city) {
+fetch("http://api.openweathermap.org/data/2.5/forecast?q=tokyo&appid=c1b5cc60aa24f5931e48ec067693e189")
+.then(response => response.json())
+.then((data) => this.displayWeather(data));
+
+}
+
+function displayWeather(data) {
+
+const {name} = data;
+const {temp} = data.main
+const {humidity} = data.main
+
+console.log(name,temp,humidity)
+
+}
+
+}
